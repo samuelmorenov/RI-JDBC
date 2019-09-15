b7f2f26f2efa645b02b32e51d56f84131645fd8f
Samuel Moreno <UO266321@uniovi.es>
2019-09-15 20:59:35 +0200

    Añadida base vista en clase

---
+++ CWS-1/src/uo/ri/ui/administrator/action/ListMechanicsAction.java
@@ -0,0 +1,13 @@
+package uo.ri.ui.administrator.action;
+
+import alb.util.menu.Action;
+
+public class ListMechanicsAction implements Action{
+
+	@Override
+	public void execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
