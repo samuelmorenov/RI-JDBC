87f04f1db5db841d78b55ed5ab8c476fb0abe847
Samuel Moreno <UO266321@uniovi.es>
2019-09-15 21:17:16 +0200

    Añadido main simple

---
+++ CWS-1/src/uo/ri/ui/Main.java
@@ -0,0 +1,10 @@
+package uo.ri.ui;
+
+import uo.ri.ui.administrator.MechanicsMenu;
+
+public class Main {
+	public static void main (String [ ] args) {
+		MechanicsMenu menu = new MechanicsMenu();
+		menu.execute();
+	}
+}
