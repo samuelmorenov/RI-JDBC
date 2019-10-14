cf93aadca91208e5b905e5f5e52e87f1460fd546
Samuel Moreno Vincent <UO266321@uniovi.es>
2019-10-14 12:03:09 +0200

    Movido listar mecanico

--- CWS/src/uo/ri/persistence/impl/MechanicGatewayImpl.java
+++ CWS/src/uo/ri/persistence/impl/MechanicGatewayImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import alb.util.jdbc.Jdbc;
@@ -57,8 +58,40 @@ public class MechanicGatewayImpl implements MechanicGateway {
 
 	@Override
 	public List<MechanicDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<MechanicDto> mechanics = null;
+		MechanicDto mechanic = null;
+
+		Connection c = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		String SQL = Conf.getInstance().getProperty("SQL_FIND_ALL_MECHANICS");
+		
+
+		try {
+			c = Jdbc.getConnection();
+			pst = c.prepareStatement(SQL);
+			rs = pst.executeQuery();
+
+			mechanics = new ArrayList<MechanicDto>();
+			while (rs.next()) {
+				mechanic = new MechanicDto();
+				mechanic.id = rs.getLong("id");// (1);
+				mechanic.dni = rs.getString("dni");
+				mechanic.name = rs.getString("name");
+				mechanic.surname = rs.getString("surname");
+
+				mechanics.add(mechanic);
+
+				// Console.printf("\t%d %s %s %s\n", rs.getLong(1);, rs.getString(2),
+				// rs.getString(3), rs.getString(4));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			Jdbc.close(rs, pst, c);
+		}
+		return mechanics;
 	}
 
 	@Override
