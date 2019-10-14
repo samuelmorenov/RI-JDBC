8c36aa9730be324ab2ecec3dc6c678f62ed5c314
Samuel Moreno Vincent <UO266321@uniovi.es>
2019-10-14 12:22:37 +0200

    Cambiado el list para adaptarse a la nueva conexion

--- CWS/src/uo/ri/business/transactionScripts/administrator/ListMechanics.java
+++ CWS/src/uo/ri/business/transactionScripts/administrator/ListMechanics.java
@@ -1,7 +1,10 @@
 package uo.ri.business.transactionScripts.administrator;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import alb.util.jdbc.Jdbc;
 import uo.ri.business.dto.MechanicDto;
 import uo.ri.persistence.MechanicGateway;
 import uo.ri.persistence.impl.MechanicGatewayImpl;
@@ -10,7 +13,12 @@ public class ListMechanics {
 
 	public List<MechanicDto> execute() {
 
-		MechanicGateway mg = new MechanicGatewayImpl();
-		return mg.findAll();
+		try (Connection c = Jdbc.getConnection();) {
+			MechanicGateway mg = new MechanicGatewayImpl();
+			mg.setConnection(c);
+			return mg.findAll();
+		} catch (SQLException e) {
+			throw new RuntimeException("Error de conexion");
+		}
 	}
 }
