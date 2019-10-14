1edd0cb086e2b9391f89db435810de618b480a09
Samuel Moreno Vincent <UO266321@uniovi.es>
2019-10-14 12:00:16 +0200

    Implementada la interfaz anterior y movido los sql a un fichero

--- CWS/src/uo/ri/business/transactionScripts/administrator/AddMechanic.java
+++ CWS/src/uo/ri/business/transactionScripts/administrator/AddMechanic.java
@@ -1,16 +1,12 @@
 package uo.ri.business.transactionScripts.administrator;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import alb.util.jdbc.Jdbc;
 import uo.ri.business.dto.MechanicDto;
+import uo.ri.persistence.MechanicGateway;
+import uo.ri.persistence.impl.MechanicGatewayImpl;
 
 public class AddMechanic {
 	
-	private static String SQL = "insert into TMechanics(dni, name, surname) values (?, ?, ?)";
+
 	private MechanicDto mechanic;
 
 	public AddMechanic(MechanicDto mechanic) {
@@ -18,27 +14,7 @@ public class AddMechanic {
 	}
 
 	public void execute() {
-		// Process
-		Connection c = null;
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-
-		try {
-			c = Jdbc.getConnection();
-			
-			pst = c.prepareStatement(SQL);
-			pst.setString(1, mechanic.dni);
-			pst.setString(2, mechanic.name);
-			pst.setString(3, mechanic.surname);
-			
-			pst.executeUpdate();
-			
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-		finally {
-			Jdbc.close(rs, pst, c);
-		}
-		
+		MechanicGateway mg = new MechanicGatewayImpl();
+		mg.add(mechanic);
 	}
 }
--- CWS/src/uo/ri/persistence/impl/MechanicGatewayImpl.java
+++ CWS/src/uo/ri/persistence/impl/MechanicGatewayImpl.java
@@ -1,15 +1,45 @@
 package uo.ri.persistence.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import alb.util.jdbc.Jdbc;
 import uo.ri.business.dto.MechanicDto;
+import uo.ri.conf.Conf;
 import uo.ri.persistence.MechanicGateway;
 
 public class MechanicGatewayImpl implements MechanicGateway {
 
 	@Override
 	public void add(MechanicDto mechanic) {
-		// TODO Auto-generated method stub
+
+		// Process
+		Connection c = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		String SQL = Conf.getInstance().getProperty("SQL_INSERT_MECHANIC");
+
+		try {
+			c = Jdbc.getConnection();
+			
+			pst = c.prepareStatement(SQL);
+			pst.setString(1, mechanic.dni);
+			pst.setString(2, mechanic.name);
+			pst.setString(3, mechanic.surname);
+			
+			pst.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			Jdbc.close(rs, pst, c);
+		}
+		
+	
 
 	}
 
