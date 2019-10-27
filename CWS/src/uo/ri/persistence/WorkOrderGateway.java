b447068a9e5a6d02a6bb8fbd5f0732be749b811e
Samuel Moreno <UO266321@uniovi.es>
2019-10-27 18:06:38 +0100

    Update del esqueleto de foreman 2

---
+++ CWS/src/uo/ri/persistence/WorkOrderGateway.java
@@ -0,0 +1,5 @@
+package uo.ri.persistence;
+
+public interface WorkOrderGateway extends Gateway {
+
+}
---
+++ CWS/src/uo/ri/persistence/impl/WorkOrderGatewayImpl.java
@@ -0,0 +1,9 @@
+package uo.ri.persistence.impl;
+
+import uo.ri.persistence.WorkOrderGateway;
+
+public class WorkOrderGatewayImpl extends GatewayImpl implements WorkOrderGateway {
+
+
+
+}
